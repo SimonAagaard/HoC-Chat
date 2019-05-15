035f30cb862adceb0b4cb768efbf2365e69e46e4
Simon Aagaard <simo136g@edu.eal.dk>
2019-05-15 20:27:59 +0200

    1st draft of styling of Login.js done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import FBLoginButton from './FBLoginButton'
 
 class Login extends Component {
     render() {
         return (
-            <View>
-            <Text>Velkommen til login skærmen!</Text>
+            <View style={styles.container}>
+                <Image 
+                style={styles.image}
+                source={require('../Components/images/Logo.png')} />
+            <Text style={styles.welcome}>Velkommen til login skærmen!</Text>
+             
               <FBLoginButton />
            
           
@@ -14,5 +18,25 @@ class Login extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        backgroundColor: '#ffffff'
+        
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginTop: '10%',
+        resizeMode: 'stretch'
+    },
+    welcome: {
+        marginBottom: '10%',
+        marginTop: '10%',
+        fontSize: 24
+    }
+})
 
 export default Login;
