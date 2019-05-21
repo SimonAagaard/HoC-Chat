c705022ec0e75bac5ccbf1493d730f2a49284975
Simon Aagaard <simo136g@edu.eal.dk>
2019-05-21 02:08:51 +0200

    added GiftedChat for send and receive messages

--- src/Views/Chat.js
+++ src/Views/Chat.js
@@ -1,27 +1,92 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
-import firebaseApp from 'react-native-firebase'
-
+import {StatusBar, View, StyleSheet} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import firebaseApp from '../Components/firebaseConfig';
 
 class Chat extends Component {
-    
-   
-    render() {
-        return (
-           <View style={styles.Container}>
-               <Text>Hi all, welcome to chat</Text>
-          
-           </View>
-        );
-    }
-}
+  
 
+//   static navigationOptions = ({ navigation }) => ({
+//     title: navigation.state.params.roomName,
+//     headerStyle: styles.messagesHeader,
+//     headerTitleStyle: styles.messagesTitle,
+//     headerBackTitleStyle: styles.messagesBackTitle
+//   });
+
+  constructor(props) {
+    super(props);
+    var FirebaseDB = firebaseApp.database();
+    var roomKey = this.props.navigation.state.params.roomKey;
+    this.messagesRef = FirebaseDB.ref(`messages/${roomKey}`);
+    this.state = {
+      user: '',
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ user: firebaseApp.auth().currentUser });
+    this.listenForMessages(this.messagesRef);
+  }
+
+  listenForMessages(messagesRef) {
+    messagesRef.on('value', (dataSnapshot) => {
+      var messagesFB = [];
+      dataSnapshot.forEach((child) => {
+        messagesFB = [({
+          _id: child.key,
+          text: child.val().text,
+          createdAt: child.val().createdAt,
+          user: {
+            _id: child.val().user._id,
+            name: child.val().user.name
+          }
+        }), ...messagesFB];
+      });
+      this.setState({ messages: messagesFB });
+    });
+  }
+
+  addMessage(messages = []) {
+    var message = messages[0]
+    this.messagesRef.push({
+      text: message.text,
+      createdAt: Date.now(),
+      user: {
+        _id: message.user._id,
+        name: message.user.name
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="light-content"/>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={this.addMessage.bind(this)}
+          user={{
+            _id: this.state.user.uid,
+            name: this.state.user.email,
+          }}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-    Container: {
-        flex:1,
-        alignItems: 'center',
-        backgroundColor: '#ffffff'
-    },
-})
+    messagesHeader: {
+        backgroundColor: '#1E90FF'
+      },
+      messagesTitle: {
+        color: '#fff',
+        fontSize: 28,
+        fontWeight: '400'
+      },
+      messagesBackTitle: {
+        color: '#fff'
+      }
+    });
 
 export default Chat;
