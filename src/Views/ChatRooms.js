0993c452fd1f7984c2f4d23f72cd678083776967
Simon Aagaard <simo136g@edu.eal.dk>
2019-05-27 16:53:33 +0200

    Clean up on master

--- src/Views/ChatRooms.js
+++ src/Views/ChatRooms.js
@@ -13,7 +13,6 @@ export default class ChatRooms extends Component {
     this.roomsRef = firebaseDB.ref('rooms');
     this.state = {
       rooms: [],
-      // newRoom: ''
     }
   }
 
@@ -49,16 +48,6 @@ export default class ChatRooms extends Component {
     this.props.navigation.navigate('Chat', { roomKey: room.key, roomName: room.name });
   }
 
-  //Checks if input field is blank, else it pushes the input to create a newroom with that name. 
-  // addRoom() {
-  //   if (this.state.newRoom === '') {
-  //     return;
-  //   }
-  //   this.roomsRef.push({ name: this.state.newRoom });
-  //   this.setState({ newRoom: '' });
-  // }
-
-
   //Used by the flatlist to format the array of data received
   renderRow(room) {
     return (
@@ -76,43 +65,26 @@ export default class ChatRooms extends Component {
 
   render() {
     return (
-      <ImageBackground source={require('../images/team.jpg')} style={{width:undefined, height:undefined,flex:1,alignSelf:'stretch', resizeMode:'cover' , opacity:0.8}} >
-      <View style={styles.roomsContainer}>
-        <StatusBar barStyle="light-content" />
-        <View style={styles.roomsHeader}>
-          <Text style={styles.roomsHeaderText}>Rooms</Text>
-          <View style={styles.roomsHeaderIcon}>
-            <TouchableHighlight onPress={() => this.handleLogout()}>
-              <Icon name='ios-log-out' color='#d7734a' size={40} />
-            </TouchableHighlight>
+      <ImageBackground source={require('../images/team.jpg')} style={{ width: undefined, height: undefined, flex: 1, alignSelf: 'stretch', resizeMode: 'cover', opacity: 0.8 }} >
+        <View style={styles.roomsContainer}>
+          <StatusBar barStyle="light-content" />
+          <View style={styles.roomsHeader}>
+            <Text style={styles.roomsHeaderText}>Rooms</Text>
+            <View style={styles.roomsHeaderIcon}>
+              <TouchableHighlight onPress={() => this.handleLogout()}>
+                <Icon name='ios-log-out' color='#d7734a' size={40} />
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View style={styles.roomsListContainer}>
+            <FlatList
+              data={this.state.rooms}
+              renderItem={({ item }) => (this.renderRow(item)
+              )}
+            />
           </View>
         </View>
-
-        {/* Section to add new rooms directly in the app */}
-
-        {/* <View style={styles.roomsInputContainer}>
-            <TextInput
-              style={styles.roomsInput}
-              placeholder={"New Room Name"}
-              onChangeText={(text) => this.setState({newRoom: text})}
-              value={this.state.newRoom}
-            />
-            <TouchableHighlight style={styles.roomsNewButton}
-              underlayColor="#fff"
-              onPress={this.addRoom()}
-            >
-              <Text style={styles.roomsNewButtonText}>Create</Text>
-            </TouchableHighlight>
-          </View> */}
-
-        <View style={styles.roomsListContainer}>
-          <FlatList
-            data={this.state.rooms}
-            renderItem={({ item }) => (this.renderRow(item)
-            )}
-          />
-        </View>
-      </View>
       </ImageBackground>
     );
   }
@@ -148,12 +120,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
     marginTop: 30,
-    // backgroundColor: '#fff'
   },
 
   roomLi: {
     flex: 1,
-    // backgroundColor: '#eee',
     borderColor: '#fff',
     borderWidth: 1,
     paddingTop: 14,
@@ -181,48 +151,4 @@ const styles = StyleSheet.create({
     textAlign: 'right',
     marginRight: 20
   },
-
-  //Styling for the "add Room" functionality
-
-  //   roomsInputContainer: {
-  //     alignItems: 'center',
-  //     flexDirection: 'row',
-  //     backgroundColor: '#fff',
-  //     borderBottomColor: '#f9f9f9',
-  //     borderBottomWidth: 2,
-  //     top: 30
-  //     },
-
-  //  roomsNewButton: {
-  //   alignItems: 'center',
-  //   marginRight: 20
-  //   },
-
-  //  roomsNewButtonText: {
-  //     color: '#1E90FF',
-  //     fontSize: 18
-  //     }
 })
-
-//BottomTab navigation bar
-// export default createMaterialBottomTabNavigator({
-//     ChatRooms: {screen:ChatRooms, 
-//     navigationOptions: {
-//       labeled: false,
-//       tabBarIcon: (
-//         <Icon name='ios-chatbubbles' color='#d7734a' size={24}/>
-//       )
-//     }
-//   },
-//     Chat: {screen:Chat,
-//       navigationOptions: {
-//         labeled: false,
-//         tabBarIcon: (
-//           <Icon name='ios-chatboxes' color='#d7734a' size={24}/>
-//         )
-//       }
-//     },
-//   },{
-//     order:['ChatRooms', 'Chat'],
-//     barStyle: {backgroundColor:'#ffffff'}
-//   })
